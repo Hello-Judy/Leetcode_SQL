b3563455acb4a766f8b38477a0e88727f59f93ab
Hello_Judy <55661351+Hello-Judy@users.noreply.github.com>
2025-04-08 12:00:48 -0500

    Time: 1393 ms (5.05%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,6 @@
 SELECT lastName, firstName, Address.city, Address.state FROM Person
 LEFT JOIN Address on Person.personId = Address.personId
 
-# Knowledge point
+# Knowledge point is Left join
+# The LEFT JOIN keyword returns all records from the left table (table1), and the matching records from the right table (table2). The result is 0 records from the right side, if there is no match.
+# So remember to add the Address.CITY and the address state
