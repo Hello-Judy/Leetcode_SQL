75b651a0698e7532f3753ae0fdbf0c087a01c980
Hello_Judy <55661351+Hello-Judy@users.noreply.github.com>
2025-04-23 10:51:12 -0500

    Time: 413 ms (78.22%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,7 +1,3 @@
-SELECT lastName, firstName, Address.city, Address.state FROM Person
-LEFT JOIN Address on Person.personId = Address.personId
-
-# Knowledge point is Left join
-# The LEFT JOIN keyword returns all records from the left table (table1), and the matching records from the right table (table2). The result is 0 records from the right side, if there is no match.
-# So remember to add the Address.CITY and the address state
-# If the left join could not find the match result, it will fill the null.
+# Write your MySQL query statement below
+select Person.lastName, Person.firstName, Address.city, Address.state
+from Person left join Address on Address.personId =  Person.personId;
